6654307933706aaa58348d65e13687fc9113ae60
TonySmol <as1994g@gmail.com>
2026-05-27 13:02:44 +0300

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const CONFIG = {
+  qdrant: {
+    url: 'https://515693ca-83f2-4a67-ba52-d44d507d8cc0.europe-west3-0.gcp.cloud.qdrant.io',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIiwic3ViamVjdCI6ImFwaS1rZXk6OGY1ZGI4Y2ItNWNkYi00MWZlLThjMjgtNGRmM2JkY2U1YzUxIn0.Vv4Xi9L_YrcFcHGcPB4EclGXGXzO1XtUzjl4AYutiPc',
+    collection: 'noosphere'
+  },
+  search: {
+    threshold: 0.3,
+    limit: 5
+  }
+};
