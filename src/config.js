7972efbcb17134ddf843b15b738d4ea18627a9f6
TonySmol <as1994g@gmail.com>
2026-05-27 13:28:37 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 export const CONFIG = {
-  qdrant: {
-    url: 'https://515693ca-83f2-4a67-ba52-d44d507d8cc0.europe-west3-0.gcp.cloud.qdrant.io',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIiwic3ViamVjdCI6ImFwaS1rZXk6OGY1ZGI4Y2ItNWNkYi00MWZlLThjMjgtNGRmM2JkY2U1YzUxIn0.Vv4Xi9L_YrcFcHGcPB4EclGXGXzO1XtUzjl4AYutiPc',
-    collection: 'noosphere'
+  repo: 'tonysmol/noosphere',  // ← ЗАМЕНИ НА СВОЙ!
+  branch: 'main',
+  worldFile: 'world.json',
+  get worldRawUrl() {
+    return `https://raw.githubusercontent.com/${this.repo}/${this.branch}/${this.worldFile}`;
   },
   search: {
     threshold: 0.3,
     limit: 5
-  }
+  },
+  excerptLen: 500
 };
