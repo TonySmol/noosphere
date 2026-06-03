2512914fe6e334641b8b50f9db9cc3eb8ec9eaf7
TonySmol <as1994g@gmail.com>
2026-06-03 09:39:45 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'noomium-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        // здесь можно перечислить важные ресурсы, но для начала хватит '/'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
